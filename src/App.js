5733bc8559f69c4c480f02f34189830d3e312745
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-10-27 14:03:30 -0400

    Fun buttons with react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const names = ['Kaylen', 'Ro', 'Tyler', 'Corwin', 'Jacques', 'Captain Ed',
+ 'Todd', 'Damian', 'Brad', 'Cari', 'Gianna', 'Eddie', 'Mido', 'Adi', 'Nick',
+ 'Fanessa', 'Gerly', 'Emily', 'Ainis', 'Sebas', 'Nerissa', 'Cas']
+
 function App() {
+  const [myName, setMyName] = useState('Tyler')
+  const [counter, setCounter] = useState(0)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {myName} is pretty damn cool!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {names.map(name => (
+          <button onClick={() => setMyName(name)}>{name}</button>
+        ))}
+        
+        <p>You clicked the button {counter} times</p>
+        <button onClick={() => setCounter(counter > 0 ? counter - 1 : 0)}>-1</button>
+        <button onClick={() => setCounter(0)}>Reset Counter</button>
+        <button onClick={() => setCounter(counter + 1)}>+1</button>
       </header>
     </div>
   );
